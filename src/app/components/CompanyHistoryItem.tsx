43760f39275eea77f4d013d9c1071246c00d075a
rajatrawat <rajatrawataku1@gmail.com>
2023-08-13 11:31:52 +0530

    fix: Adding padding to work and projects list items

--- src/app/components/CompanyHistoryItem.tsx
+++ src/app/components/CompanyHistoryItem.tsx
@@ -20,9 +20,11 @@ export default function HistoryItem({
       <div className="flex flex-col gap-2">
         <div className="text-3xl font-bold">{title}</div>
         <div className="text-2xl">{company}</div>
-        <ul className="list-disc marker:text-primaryLight list-inside">
+        <ul className="list-disc marker:text-primaryLight p-4 list-outside">
           {contentItems.map(content => (
-            <li key={content}>{content}</li>
+            <li key={content} className="py-1 first:pt-0 last:pb-0">
+              {content}
+            </li>
           ))}
         </ul>
         {time && (
--- src/app/components/ProjectsItem.tsx
+++ src/app/components/ProjectsItem.tsx
@@ -17,9 +17,11 @@ export default function HistoryItem({
     <div className="flex gap-4 font-sans">
       <div className="flex flex-col gap-2">
         <div className="text-2xl font-semi-bold">{title}</div>
-        <ul className="list-disc marker:text-primaryLight list-inside">
+        <ul className="list-disc marker:text-primaryLight list-outside p-4">
           {contentItems.map(content => (
-            <li key={content}>{content}</li>
+            <li key={content} className="py-1 first:pt-0 last:pb-0">
+              {content}
+            </li>
           ))}
         </ul>
         {time && (
